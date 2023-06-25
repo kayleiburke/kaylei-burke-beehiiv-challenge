ba97e9e579eee36928e5e5788365a5c2df48e650
Kaylei Burke <kaylei.burke@gmail.com>
2023-06-25 16:23:17 -0500

    Populate seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+subscribers = [
+  {
+    name: "Rick Sanchez",
+    email: "rickc137@citadel.com",
+    status: "active"
+  },
+  {
+    name: "Morty Smith",
+    email: "morty.smith@gmail.com",
+    status: "inactive"
+  },
+  {
+    name: "Jerry Smith",
+    email: "jerry.smith@aol.com",
+    status: "active"
+  },
+  {
+    name: "Beth Smith",
+    email: "beth.smith@gmail.com",
+    status: "active"
+  },
+  {
+    name: "Summer Smith",
+    email: "summer.smith@gmail.com",
+    status: "active"
+  },
+  {
+    name: "Bird Person",
+    email: "bird.person@birdworld.com",
+    status: "active"
+  }
+]
+
+subscribers.each do |subscriber_data|
+  Subscriber.create!(
+    name: subscriber_data[:name],
+    email: subscriber_data[:email],
+    status: subscriber_data[:status]
+  )
+end
